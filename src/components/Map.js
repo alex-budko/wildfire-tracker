f125140ebb5aae86c27c162d4cc67aa884334ca7
alex-budko <xalex.budko@gmail.com>
2022-04-30 22:49:43 -0400

    implemented locationInfoBox

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,23 @@
 import GoogleMapReact from 'google-map-react';
-import React from 'react';
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
+import {useState} from 'react'
+
 
 //const MY_KEY = process.env.REACT_APP_API_KEY;
 const Map = ({eventData, center, zoom}) => {
+  const [locationInfo, setLocationInfo] = useState(null)
+
   const markers = eventData.map(ev => {
     if (ev.categories[0].id === 8) {
-      return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+      return 
+      <LocationMarker 
+        lat={ev.geometries[0].coordinates[1]} 
+        lng={ev.geometries[0].coordinates[0]}
+        onClick={()=>setLocationInfo({id: ev.id, title: ev.title})}
+       />
     }
+    return null
   })
 
   return (
@@ -23,6 +33,7 @@ const Map = ({eventData, center, zoom}) => {
         >
           {markers}
         </GoogleMapReact>
+        {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   )
 }
